67d82618ac86c3113cf5f6268f2d40af168039e7
matjcqm <mathieu.jacquemin@hotmail.com>
2023-01-16 15:40:02 +0100

    404 page OK + all links OK

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,20 @@ import Banner from '../../components/Banner/Banner';
 import AccommodationCard from '../../components/AccommodationCard/AccommodationCard'
 import styles from './Home.module.css';
 import data from '../../data'
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
     return (
-      <div>
+      <>
           <Banner />
           <div className={styles.accommodationsList}>
             {data.map((accommodation) =>
+            <Link to={`/acommodation/${accommodation.id}`}>
               <AccommodationCard logement={accommodation} key={accommodation.id} />
+            </Link>
             )}
           </div>
-      </div>
+      </>
     );
   }
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,17 @@
 import styles from './NotFound.module.css'
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-      <div>
-          <h1 className={styles.title}>Erreur 404</h1>
+      <div className={styles.notFound}>
+          <h1 className={styles.error}>404</h1>
+          <div className={styles.texts}>
+            <h2 className={styles.text}>Oups! La page que&nbsp;</h2>
+            <h2 className={styles.text}>vous demandez n'éxiste pas.</h2>
+          </div>
+          <p className={styles.link}>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+          </p>
       </div>
     );
   }
